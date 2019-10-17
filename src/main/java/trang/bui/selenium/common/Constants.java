21c503263cee6cbc004617a870b4cbb3e209960b
Dung.Vo <dung.vo@jeppesen.com>
2019-10-17 14:24:59 +0700

    Add Constant class

---
+++ src/main/java/trang/bui/selenium/common/Constants.java
@@ -0,0 +1,12 @@
+package trang.bui.selenium.common;
+
+public class Constants {
+    public static final int TITLE_BOX_WIDTH = 70;
+    public static final int TITLE_TEXT_WIDTH = 25;
+    public static final int WAIT_3 = 3;
+    public static final int WAIT_10 = 10;
+    public static final int WAIT_20 = 20;
+    public static final int WAIT_60 = 60;
+    public static final int WAIT_120= 120;
+    public static final int WAIT_300 = 300;
+}
