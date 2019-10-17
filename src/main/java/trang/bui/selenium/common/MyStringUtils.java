43f448a2231c4690f71ddd338e6b8b9f541ac147
Dung.Vo <dung.vo@jeppesen.com>
2019-10-17 14:25:23 +0700

    Add MyStringUtils class in common

---
+++ src/main/java/trang/bui/selenium/common/MyStringUtils.java
@@ -0,0 +1,36 @@
+package trang.bui.selenium.common;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MyStringUtils {
+    //Default Constructor
+    private MyStringUtils() {}
+
+    /**
+     * Repeat * num times.
+     * @param num
+     * @return * num times, *******
+     */
+    public static String asterisk(int num) {
+        return StringUtils.repeat("*", num);
+    }
+
+    public static String asterisk() {
+        return StringUtils.repeat("*", Constants.TITLE_BOX_WIDTH);
+    }
+
+
+    public static String centerTitleWith$(String title, int size, String padStr) {
+
+        return StringUtils.center(StringUtils.center(title, Constants.TITLE_TEXT_WIDTH, ' '), size, padStr);
+    }
+
+    public static String centerTitleWith$(String title) {
+        return centerTitleWith$(title, Constants.TITLE_BOX_WIDTH, "$");
+    }
+
+    public static String centerTitleWithDash(String title) {
+        return centerTitleWith$(title, Constants.TITLE_BOX_WIDTH, "=");
+    }
+
+}
