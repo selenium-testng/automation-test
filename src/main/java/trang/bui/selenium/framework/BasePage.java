365c55b785aa29c6b42b4b23d03ad0f7edff7bb4
Dung.Vo <dung.vo@jeppesen.com>
2019-10-21 09:46:34 +0700

    Create BasePage for all pages later

---
+++ src/main/java/trang/bui/selenium/framework/BasePage.java
@@ -0,0 +1,47 @@
+package trang.bui.selenium.framework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import trang.bui.selenium.common.Constants;
+
+public class BasePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Constants.WAIT_10, Constants.WAIT_120);
+    }
+
+    protected void waitForElementToAppear(By element) {
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(element));
+    }
+
+
+    protected void waitForElementToDisappear(By element) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(element));
+    }
+
+    protected void writeText(By element, String text) {
+        waitForElementToAppear(element);
+        driver.findElement(element).sendKeys(text);
+    }
+
+    protected String readText(By element) {
+        waitForElementToAppear(element);
+        return driver.findElement(element).getText();
+    }
+
+    protected void click (By element) {
+        waitForElementToAppear(element);
+        driver.findElement(element).click();
+    }
+
+    protected void assertEquals(By element, String expectedText) {
+        waitForElementToAppear(element);
+        Assert.assertEquals(readText(element), expectedText);
+    }
+}
